c0f6835fda47e701fadeeb2b7d4d3c4a16ad99ab
jinhyeok <ekdrn3330@naver.com>
2021-10-13 17:18:58 +0900

    211013 코틀린 공부 시작

---
+++ app/src/main/java/com/example/kotlinsample/ControlKotlinActivity.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinsample
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ControlKotlinActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_control)
+    }
+}
